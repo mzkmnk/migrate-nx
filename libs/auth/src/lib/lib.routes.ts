edd3bea36082da7f72822427107693ee8eb04ca8
mzkmnk <sma0729mex@gmail.com>
2025-02-15 17:58:03 +0900

    feat(sign-in): create sign-in with an alias

--- libs/auth/src/lib/lib.routes.ts
+++ libs/auth/src/lib/lib.routes.ts
@@ -2,6 +2,10 @@ import { Route } from '@angular/router';
 
 export const authRoutes: Route[] = [
   {
+    path:'sign-in',
+    loadComponent:() => import('@libs/auth/sign-in').then((M) => M.SignInComponent),
+  },
+  {
     path:'**',
     redirectTo: 'sign-in',
   }
